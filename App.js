faac3241d3a9456a53b9d8e8943eea90cef87b10
pailita842 <paulita842@gmail.com>
2021-05-02 16:46:15 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, View , Image, Text, ScrollView } from 'react-native';
+
+const App = ()=>  {
+  return (
+    <>
+      
+    <ScrollView>
+
+      <View style={{ flexDirection: 'row' }}>
+        <Image
+          style={styles.banner}
+          source={ require('./assets/img/bg.jpg') }
+        />
+      </View>
+
+      <View style={styles.contenedor}>
+           <Text style={styles.titulo}>Qué hacer en Paris</Text>
+            <ScrollView
+              horizontal
+            >
+          <View>
+            <Image
+              style={styles.ciuda}
+              source={ require('./assets/img/actividad1.jpg') }
+            />
+             <Image
+              style={styles.ciuda}
+              source={ require('./assets/img/actividad2.jpg') }
+            />
+             <Image
+              style={styles.ciuda}
+              source={ require('./assets/img/actividad3.jpg') }
+            />
+             <Image
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad4.jpg') }
+            />
+             <Image
+              style={styles.ciudad}
+              source={ require('./assets/img/actividad5.jpg') }
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </ScrollView>
+    </>
+    );
+};
+
+const styles = StyleSheet.create({
+  banner:{
+    height: 250,
+    flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  }, 
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
+  }
+  
+
+});
+
+export default App;
